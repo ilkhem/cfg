1c99908a2fa0eb8b7b0736d7b13b1b5b6e7b10a6
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-01-04 18:18:05 +0000

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,128 @@
+
+### EXPORT ###
+# set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
+set -x PATH $HOME/.local/bin $HOME/Applications $PATH
+set fish_greeting                      # Supresses fish's intro message
+# set TERM "xterm-256color"              # Sets the terminal type
+set EDITOR "nvim"      # $EDITOR use Emacs in terminal
+set VISUAL "emacsclient -c -a emacs"   # $VISUAL use Emacs in GUI mode
+
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+### END OF VI MODE ###
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+# set fish_color_normal brcyan
+# set fish_color_autosuggestion '#7d7d7d'
+# set fish_color_command brcyan
+# set fish_color_error '#ff6c6b'
+# set fish_color_param brcyan
+# set -L
+set -U fish_color_normal normal
+set -U fish_color_command c397d8
+set -U fish_color_quote b9ca4a
+set -U fish_color_redirection 70c0b1
+set -U fish_color_end c397d8
+set -U fish_color_error d54e53
+set -U fish_color_param 7aa6da
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 969896
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_match --background=brblue
+set -U fish_color_comment e7c547
+set -U fish_color_normal normal
+
+
+### FUNCTIONS ###
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ $fish_key_bindings = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_comman_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# # Function for copying files and directories, even recursively.
+# # ex: copy DIRNAME LOCATIONS
+# # result: copies the directory and all of its contents.
+# function copy
+#     set count (count $argv | tr -d \n)
+#     if test "$count" = 2; and test -d "$argv[1]"
+# 	set from (echo $argv[1] | trim-right /)
+# 	set to (echo $argv[2])
+#         command cp -r $from $to
+#     else
+#         command cp $argv
+#     end
+# end
+### END OF FUNCTIONS ###
+
+### ALIASES ###
+source ~/.aliases
+
+
+# run ssh-agent to hold private keys
+# if [[ -n $SSH_CONNECTION ]]; then
+#         eval `ssh-agent -s` > /dev/null
+# fi
+
+# disable opencv warnings in howdy
+set OPENCV_LOG_LEVEL ERROR
+
+### RANDOM COLOR SCRIPT ###
+colorscript random
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
+
+
+### configure pyenv path
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+# pyenv init - | source
+# pyenv virtualenv-init - | source
