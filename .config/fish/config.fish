8a71eb6ba9c93ec60a42e7400db2b0022a8f101f
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-01-18 22:40:58 +0000

    update colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,32 +21,65 @@ end
 # set fish_color_error '#ff6c6b'
 # set fish_color_param brcyan
 # set -L
+# tomorrow night bright theme
+# set -U fish_color_normal normal
+# set -U fish_color_command c397d8
+# set -U fish_color_quote b9ca4a
+# set -U fish_color_redirection 70c0b1
+# set -U fish_color_end c397d8
+# set -U fish_color_error d54e53
+# set -U fish_color_param 7aa6da
+# set -U fish_color_selection white --bold --background=brblack
+# set -U fish_color_search_match bryellow --background=brblack
+# set -U fish_color_history_current --bold
+# set -U fish_color_operator 00a6b2
+# set -U fish_color_escape 00a6b2
+# set -U fish_color_cwd green
+# set -U fish_color_cwd_root red
+# set -U fish_color_valid_path --underline
+# set -U fish_color_autosuggestion 969896
+# set -U fish_color_user brgreen
+# set -U fish_color_host normal
+# set -U fish_color_cancel -r
+# set -U fish_pager_color_completion normal
+# set -U fish_pager_color_description B3A06D yellow
+# set -U fish_pager_color_prefix white --bold --underline
+# set -U fish_pager_color_progress brwhite --background=cyan
+# set -U fish_color_match --background=brblue
+# set -U fish_color_comment e7c547
+# set -U fish_color_normal normal
+
+
+# a modifed base color theme
+# set -L
 set -U fish_color_normal normal
-set -U fish_color_command c397d8
-set -U fish_color_quote b9ca4a
-set -U fish_color_redirection 70c0b1
-set -U fish_color_end c397d8
-set -U fish_color_error d54e53
-set -U fish_color_param 7aa6da
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
+set -U fish_color_command 00ffd7
+set -U fish_color_quote ffd700
+set -U fish_color_redirection d787ff
+set -U fish_color_end 00ff5f
+set -U fish_color_error ff5f5f
+set -U fish_color_param 00afff
+set -U fish_color_selection c0c0c0
+set -U fish_color_search_match ffff00
+set -U fish_color_history_current normal
 set -U fish_color_operator 00a6b2
 set -U fish_color_escape 00a6b2
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 969896
-set -U fish_color_user brgreen
+set -U fish_color_cwd 008000
+set -U fish_color_cwd_root 800000
+set -U fish_color_valid_path normal
+set -U fish_color_autosuggestion 6c6c6c
+set -U fish_color_user 00ff00
 set -U fish_color_host normal
-set -U fish_color_cancel -r
+set -U fish_color_cancel normal
 set -U fish_pager_color_completion normal
 set -U fish_pager_color_description B3A06D yellow
 set -U fish_pager_color_prefix white --bold --underline
 set -U fish_pager_color_progress brwhite --background=cyan
-set -U fish_color_match --background=brblue
-set -U fish_color_comment e7c547
-set -U fish_color_normal normal
+set -U fish_color_match normal
+set -U fish_color_comment ffd700
+
+
+
 
 
 ### FUNCTIONS ###
@@ -105,9 +138,9 @@ source ~/.aliases
 
 
 # run ssh-agent to hold private keys
-# if [[ -n $SSH_CONNECTION ]]; then
-#         eval `ssh-agent -s` > /dev/null
-# fi
+# if [ -n $SSH_CONNECTION ]
+#         eval (ssh-agent -c)
+# end
 
 # disable opencv warnings in howdy
 set -x OPENCV_LOG_LEVEL ERROR
