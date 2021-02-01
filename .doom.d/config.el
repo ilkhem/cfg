8a21e4135cc8af4a3db120a79c8b2890e8401146
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-02-01 22:48:26 +0000

    doom: add general configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -4,7 +4,7 @@
 ;; sync' after modifying this file!
 
 ;; ------------------------------------------------------------------------------
-;; GLOBALS AND DEFAULTS
+;;;;;;;;;; GLOBALS AND DEFAULTS ;;;;;;;;;
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
@@ -16,35 +16,69 @@
 
 (setq doom-theme 'doom-one)
 (setq display-line-numbers-type 'relative)
+(display-time-mode 1)
+(setq-default window-combination-resize t)
+(setq which-key-idle-delay 0.5)
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+(map! :n [mouse-8] #'better-jumper-jump-backward
+      :n [mouse-9] #'better-jumper-jump-forward)
+(custom-set-faces! '(doom-modeline-buffer-modified :foreground "orange"))
+(setq global-hl-todo-mode t)
+(map! :leader
+      :desc "Load new theme"
+      "h t" #'counsel-load-theme)
+
 
 
 
 
 
 ;; ------------------------------------------------------------------------------
-;; ORG
+;;;;;;;;;;;;; GENERAL PACKAGE CONFIG ;;;;;;
+
 ;;
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+;; DIRED
+
+(map! :leader
+      :desc "Dired"
+      "d d" #'dired
+      :leader
+      :desc "Dired jump to current"
+      "d j" #'dired-jump
+      )
+(evil-define-key 'normal dired-mode-map
+  (kbd "h") 'dired-up-directory
+  (kbd "l") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
+
+
+;; ------------------------------------------------------------------------------
+;; COMPANY
+(after! company
+  (setq company-idle-delay 0.2
+        company-minimum-prefix-length 2))
+
+
+
+;;;;;;;;;; LANGUAGES ;;;;;;;;;;;
+
+;; ------------------------------------------------------------------------------
+;; PYTHON
+
+; Nothing to configure for python, everything works out-of-the box after installing the lsp server.
+
+
+
+;; ------------------------------------------------------------------------------
+;; ORG
+
+(setq deft-directory "~/notes"
+      deft-extensions '("org" "txt")
+      deft-recursive t)
+
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;; ------------------------------------------------------------------------------
+;; LATEX
