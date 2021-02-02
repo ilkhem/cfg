c0d1f05a5eba2d86335317075cf0923cd549cc4a
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-02-02 00:11:03 +0000

    doom: reftex work in org

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -57,6 +57,7 @@
 (after! company
   (setq company-idle-delay 0.2
         company-minimum-prefix-length 2))
+(add-hook 'after-init-hook 'global-company-mode)
 
 
 
@@ -76,8 +77,72 @@
       deft-extensions '("org" "txt")
       deft-recursive t)
 
-(setq org-directory "~/org/")
+(defun org-mode-reftex-setup ()
+  (load-library "reftex")
+  (and (buffer-file-name)
+       (file-exists-p (buffer-file-name))
+       (reftex-parse-all))
+  (define-key org-mode-map (kbd "C-c )") 'reftex-citation))
 
+(setq reftex-default-bibliography (quote ("~/texmf/bibtex/bib/refs_zot.bib")))
+
+(after! org
+  (add-hook 'org-mode-hook 'org-mode-reftex-setup)
+  ;; (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
+  ;;   (defadvice! org-edit-latex-emv-after-insert ()
+  ;;     :after #'org-cdlatex-environment-indent
+  ;;     (org-edit-latex-environment))
+  (setq org-directory "~/notes/"
+        org-use-property-inheritance t  ; it's convenient to have properties inherited
+        ;; org-export-in-background t        ; run export processes in external emacs process
+        ;; org-catch-invisible-edits 'smart  ; try not to accidently do weird stuff in invisible regions
+        org-agenda-files '("~/notes/agenda.org" "~/notes/tasks.org")
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▾ "
+        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
+          '((sequence "TODO(t)" "INPROGRESS(i)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)" ))
+        org-todo-keyword-faces
+          '(("TODO" :foreground "#7c7c75" :weight normal :underline t)
+            ("WAITING" :foreground "#9f7efe" :weight normal :underline t)
+            ("INPROGRESS" :foreground "#0098dd" :weight normal :underline t)
+            ("DONE" :foreground "#50a14f" :weight normal :underline t)
+            ("CANCELLED" :foreground "#ff6480" :weight normal :underline t))
+        org-log-done 'time
+        org-journal-dir "~/notes/journal/"
+        org-journal-date-prefix "#+TITLE: "
+        org-journal-time-prefix "* "
+        org-journal-date-format "%A, %d %b %Y"
+        org-journal-file-format "%Y-%m-%d.org"
+        org-hide-emphasis-markers t
+        )
+  (custom-set-faces!
+    '(outline-1 :weight extra-bold :height 1.2)
+    '(outline-2 :weight bold :height 1.15)
+    '(outline-3 :weight bold :height 1.12)
+    '(outline-4 :weight semi-bold :height 1.09)
+    '(outline-5 :weight semi-bold :height 1.06)
+    '(outline-6 :weight semi-bold :height 1.03)
+    '(outline-8 :weight semi-bold)
+    '(outline-9 :weight semi-bold)
+    '(org-document-title :height 1.2))
+  ;; (setq org-latex-pdf-process '("latexmk -f -pdf %f"))
+  (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f"))
+  ;; (setq org-latex-pdf-process '("latexmk -%latex -shell-escape -interaction=nonstopmode -f -pdf -output-directory=%o %f"))
+  (require 'ox-extra)
+  (ox-extras-activate '(ignore-headlines))
+  (require 'ox-latex)
+  (unless (boundp 'org-latex-classes)
+    (setq org-latex-classes nil))
+  (add-to-list 'org-latex-classes
+             '("article"
+               "\\documentclass{article}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (setq org-export-latex-hyperref-format "\\ref{%s}")
+  )
 
 
 ;; ------------------------------------------------------------------------------
---
+++ .doom.d/custom.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/notes/org-mode-test.org" "~/notes/agenda.org" "~/notes/tasks.org"))
+ '(reftex-cite-format
+   '((97 . "\\autocite[]{%l}")
+     (98 . "\\blockcquote[]{%l}{}")
+     (99 . "\\cite[]{%l}")
+     (102 . "\\footcite[]{%l}")
+     (110 . "\\nocite{%l}")
+     (112 . "\\citep[]{%l}")
+     (115 . "\\smartcite[]{%l}")
+     (116 . "\\citet[]{%l}"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "orange"))))
+ '(org-document-title ((t (:height 1.2))))
+ '(outline-1 ((t (:weight extra-bold :height 1.2))))
+ '(outline-2 ((t (:weight bold :height 1.15))))
+ '(outline-3 ((t (:weight bold :height 1.12))))
+ '(outline-4 ((t (:weight semi-bold :height 1.09))))
+ '(outline-5 ((t (:weight semi-bold :height 1.06))))
+ '(outline-6 ((t (:weight semi-bold :height 1.03))))
+ '(outline-8 ((t (:weight semi-bold))))
+ '(outline-9 ((t (:weight semi-bold)))))
