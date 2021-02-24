a3908fa3bc171bc1e68d05d18b2c6bb6200c0eff
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-02-24 16:23:27 +0000

    improve autostart script

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+# Helper function that checks if a command is already running
 function runonce() {
         pgname=$(echo "$1" | awk '{print $1}')
         pgrep -u $USER -x $pgname > /dev/null || $1
 }
 
+# Commands that need to run in the background
 runonce "picom" &
 runonce "setbg" &
 runonce "dwmblocks" &
 runonce "powerkit" &
-runonce "xcape -e Caps_Lock=Escape" &
-runonce "lxsession" &
+runonce "lxpolkit" &
+
+# Commands that are only run once
+autorandr -c --default default &
+remaps &
