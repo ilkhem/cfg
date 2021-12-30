10d2eae85bd3d5d9de475e8e72eb5b59d0a9d818
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-12-30 16:16:00 +0000

    update custom bin scripts

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -9,8 +9,9 @@ function runonce() {
 # Commands that run in the background shouldn't be restarted when dwm is restared ---> use runonce
 runonce "picom" &
 runonce "dwmblocks" &
-runonce "powerkit" &
+# runonce "powerkit" &
 runonce "lxpolkit" &
+runonce "dunst" &
 autorandr -c --default default &
 setbg &
 remaps & # remaps calls xcape with "killall xcape; xcape" so that only one instance is running, even on dwm restart
